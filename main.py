beb74856e93bc2c06e58892a7c7dc22878a349cf
endail <52652357+endail@users.noreply.github.com>
2022-12-25 22:15:13 +1100

    doc update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from machine import Pin
 from src.hx711 import *
 
-# 1. initalise the hx711 with pin 4 as clock pin, pin
-# 5 as data pin
-hx = hx711(Pin(4), Pin(5))
+# 1. initalise the hx711 with pin 14 as clock pin, pin
+# 15 as data pin
+hx = hx711(Pin(14), Pin(15))
 
 # 2. power up
 hx.set_power(hx711.power.pwr_up)
